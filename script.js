3dccd777f466e3496424a0980a15c5dd2ca68847
Maykid24 <patjster@gmail.com>
2016-04-27 16:58:34 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function sumone(a, b) {
+	var a = 5;
+	var b = 5;
+
+	var x = a + b;
+	alert(x);
+}
+	function sumtwo() {
+		var a = 5;
+		var b = 5;
+		var z = 20;
+		var x = z * a / b;
+		alert(x);
+	}
+		function sumthree(){
+			var a = 5;
+			var b = 5;
+			var x = 4;
+			var y = x * a * b;	
+
+		alert(y);
+		}
+
